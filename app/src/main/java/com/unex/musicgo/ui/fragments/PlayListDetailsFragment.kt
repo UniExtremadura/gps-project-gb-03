29cc3cf462a47860a576138740dc455c45947e3d
ManuelDuque <manudunu@gmail.com>
2023-11-24 10:38:55 +0100

    GG-33 IT2 - I - UC07 - Implement Components
    Updated PlayListDetailsFragment.
    Updated SongListAdapter to show trash and handle click.
    Updated SongListFragment.
    Added dialog translations.

--- app/src/main/java/com/unex/musicgo/ui/fragments/PlayListDetailsFragment.kt
+++ app/src/main/java/com/unex/musicgo/ui/fragments/PlayListDetailsFragment.kt
@@ -1,5 +1,6 @@
 package com.unex.musicgo.ui.fragments
 
+import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
@@ -13,12 +14,14 @@ import androidx.lifecycle.lifecycleScope
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.unex.musicgo.R
 import com.unex.musicgo.database.MusicGoDatabase
+import com.unex.musicgo.databinding.DialogBinding
 import com.unex.musicgo.databinding.ListDisplayBinding
 import com.unex.musicgo.models.PlayList
 import com.unex.musicgo.models.PlayListWithSongs
+import com.unex.musicgo.models.Song
 import kotlinx.coroutines.launch
 
-class PlayListDetailsFragment : Fragment() {
+class PlayListDetailsFragment : Fragment(), SongListFragment.OnSongDeleteListener {
 
     private val TAG = "PlayListDetailsFragment"
 
@@ -311,6 +314,38 @@ class PlayListDetailsFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null // avoid memory leaks
+    }
+
+    override fun onSongDelete(song: Song) {
+        Log.d(TAG, "onDeleteSongFromPlayListClick")
+
+        // Show a dialog to confirm the deletion of the song
+        val dialog = Dialog(requireContext())
+        val dialogBinding = DialogBinding.inflate(layoutInflater)
+        dialog.setContentView(dialogBinding.root)
+
+        with(dialogBinding) {
+            dialogMessage.text = getString(R.string.dialog_delete_song_from_list)
+            dialogPlaylistName.text = song.title
+
+            confirmButton.setOnClickListener {
+                lifecycleScope.launch {
+                    playlist?.let {
+                        db?.playListSongCrossRefDao()?.delete(it.id, song.id)
+                        Log.d(TAG, "Song deleted from playlist")
+                        this@PlayListDetailsFragment.bind()
+                    }
+                }
+                dialog.dismiss()
+            }
+
+            cancelButton.setOnClickListener {
+                dialog.dismiss()
+            }
+        }
+
+        // Show the dialog
+        dialog.show()
     }
 
 }
