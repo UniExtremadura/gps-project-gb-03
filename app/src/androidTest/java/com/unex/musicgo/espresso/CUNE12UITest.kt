08a843c7df001c6355147ce0d79f4cc21b161faa
ruben <rbarruec@alumnos.unex.es>
2023-12-16 22:45:34 +0100

    GG-154 IT6 - INT - UC12 - Test and Evaluate
    Added CUNE-12 espresso test.

---
+++ app/src/androidTest/java/com/unex/musicgo/espresso/CUNE12UITest.kt
@@ -0,0 +1,32 @@
+package com.unex.musicgo.espresso
+
+
+import android.widget.EditText
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.pressImeActionButton
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.unex.musicgo.ui.activities.HomeActivity
+import org.hamcrest.core.AllOf.allOf
+import org.junit.Rule
+import org.junit.Test
+import com.unex.musicgo.R
+
+
+class CUNE12UITest {
+
+
+    private fun sleepThread(seconds: Int) {
+        try {
+            val milliseconds = seconds * 1000L
+            Thread.sleep(milliseconds)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+    }
+}
