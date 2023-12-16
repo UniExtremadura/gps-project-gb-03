c4e32ca4e0611f5599aaefe9b929adf522f5bfa2
AnabelGonzalezFelix <77212745+AnabelGonzalezFelix@users.noreply.github.com>
2023-12-16 21:25:47 +0100

    GG-93 IT3 - INT - UC14 - Test and Evaluate
    Added CUNE-14 espresso test.

--- app/src/androidTest/java/com/unex/musicgo/espresso/CUNE14UITest.kt
+++ app/src/androidTest/java/com/unex/musicgo/espresso/CUNE14UITest.kt
@@ -1,11 +1,16 @@
 package com.unex.musicgo.espresso
 
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.RootMatchers
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.unex.musicgo.ui.activities.HomeActivity
+import org.hamcrest.core.AllOf
 import org.junit.Rule
+import org.junit.Test
 import com.unex.musicgo.R
 import org.junit.Before
 
@@ -45,6 +50,22 @@ class CUNE14UITest {
         } catch (e: InterruptedException) {
             e.printStackTrace()
         }
+    }
+    @Test
+    fun testDeletePlaylist(){
+        Espresso.onView(AllOf.allOf(ViewMatchers.withId(R.id.recycler_view)))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, ViewActions.click()))
+        sleepThread(1)
+
+        Espresso.onView(ViewMatchers.withId(R.id.trash_icon)).perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.confirm_button))
+            .inRoot(RootMatchers.isDialog())
+            .perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.top_list)).perform(ViewActions.click())
+        sleepThread(1)
+
     }
 
 }
